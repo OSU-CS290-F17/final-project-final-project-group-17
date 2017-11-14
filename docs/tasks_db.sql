d1aa0f7c58c365bd848847ab8d44cf4674c6a26b
Casey Dinsmore <lunitin@gmail.com>
2017-11-14 14:45:00 -0800

    Added Documentation folder and some DB documentation

---
+++ docs/tasks_db.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.6
+-- http://www.phpmyadmin.net
+--
+-- Host: rds1.c9cmpaaxofup.us-west-2.rds.amazonaws.com
+-- Generation Time: Nov 14, 2017 at 02:34 PM
+-- Server version: 5.5.53-log
+-- PHP Version: 5.3.29
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tasks`
+--
+
+CREATE TABLE IF NOT EXISTS `tasks` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `task_group` enum('A','B','C','D','E') NOT NULL DEFAULT 'A',
+  `task_title` varchar(255) DEFAULT NULL,
+  `task_details` text,
+  `date_added` datetime DEFAULT NULL,
+  `date_due` datetime DEFAULT NULL,
+  `date_completed` datetime DEFAULT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
