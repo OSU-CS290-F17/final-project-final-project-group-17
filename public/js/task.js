c572ab897c7b437011afdc66421becc2395e89eb
Casey Dinsmore <lunitin@gmail.com>
2017-11-14 16:55:01 -0800

    Fixed typo

--- public/js/task.js
+++ public/js/task.js
@@ -3,12 +3,12 @@
  */
 
 function Task(task_id, task_group, task_title, task_details, task_priority, date_added, date_due, date_done) {
-  this.task_id       = task_id ;
-  this.task_group    = task_group ;
-  this.task_title    = task_title ;
+  this.task_id       = task_id;
+  this.task_group    = task_group;
+  this.task_title    = task_title;
   this.task_details  = task_details;
   this.task_priority = task_priority;
-  this.date_added    = date_add ed;
-  this.date_due      = date_due ;
-  this.date_done     = date_done ;
+  this.date_added    = date_added;
+  this.date_due      = date_due;
+  this.date_done     = date_done;
 }
