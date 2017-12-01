e2f13784871dfa689436caf0dca4be346a9a48b2
Kristen <patterkr@oregonstate.edu>
2017-11-30 18:00:01 -0800

    Did more work on modal

--- docs/index.js
+++ docs/index.js
@@ -33,9 +33,48 @@ function modalHide() {
   	clearInputs();
 }
 
+function modalAccept(group) {
+	var shortDesc = document.getElementById('add-task-short');
+	var date = document.getElementById('add-task-date');
+	var priority = document.getElementById('add-task-prio');
+	var longDesc = document.getElementById('add-task-long');
+
+	if(!shortDesc || !date || !priority || !longDesc){
+		alert("All fields must be entered properly");
+	}
+	else{
+		//handle sending data to server and posting
+	}
+}
+
 function taskAEventHandle() {
 	modalVisible;
 	var group = 'A';
+	modalAccept(group);
+}
+
+function taskBEventHandle() {
+	modalVisible;
+	var group = 'B';
+	modalAccept(group);
+}
+
+function taskCEventHandle() {
+	modalVisible;
+	var group = 'C';
+	modalAccept(group);
+}
+
+function taskDEventHandle() {
+	modalVisible;
+	var group = 'D';
+	modalAccept(group);
+}
+
+function taskEEventHandle() {
+	modalVisible;
+	var group = 'E';
+	modalAccept(group);
 }
 
 var cancelModal = document.getElementById('cancel-modal-button');
@@ -48,3 +87,7 @@ var addTaskD = document.getElementById('add-task-D');
 var addTaskE = document.getElementById('add-task-E');
 
 addTaskA.addEventListener('click', taskAEventHandle);
+addTaskB.addEventListener('click', taskBEventHandle);
+addTaskC.addEventListener('click', taskCEventHandle);
+addTaskD.addEventListener('click', taskDEventHandle);
+addTaskE.addEventListener('click', taskEEventHandle);
